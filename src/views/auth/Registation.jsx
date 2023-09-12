931ed247a407a4fc45f4434dae31d0f62ba7e400
joydas <joy600508@gmail.com>
2023-09-13 05:33:15 +0600

    login and registation form added

--- src/views/auth/Registation.jsx
+++ src/views/auth/Registation.jsx
@@ -1,30 +1,23 @@
 import React from 'react'
 import { BiLogoFacebookCircle, BiLogoGithub, BiLogoGoogle } from 'react-icons/bi';
-import { FcGoogle } from 'react-icons/fc';
 import { AiFillTwitterCircle } from 'react-icons/ai';
 
 const Registation = () => {
     return (
-        <div className="relative flex flex-col justify-center h-screen overflow-y-scroll">
-            <div className="w-full p-6 m-auto bg-white rounded-md shadow-md ring-2 ring-gray-800/50 lg:max-w-xl">
-                <h1 className="text-3xl font-semibold text-center text-gray-700">Wellcome to e-commerce</h1>
+        <div className="relative flex flex-col justify-center h-screen overflow-y-s">
+            <div className="w-full my-10 p-6 m-auto bg-white rounded-md shadow-md ring-2 ring-gray-800/50 lg:max-w-xl">
+                <h1 className="text-3xl font-semibold text-center text-gray-700 mb-6">Wellcome to e-commerce</h1>
                 <form className="space-y-4">
                     <div>
-                        <label className="label">
-                            <span className="text-base label-text">Name</span>
-                        </label>
+
                         <input type="text" placeholder="Name" className="w-full input input-bordered" />
                     </div>
                     <div>
-                        <label className="label">
-                            <span className="text-base label-text">Email</span>
-                        </label>
+
                         <input type="text" placeholder="Email Address" className="w-full input input-bordered" />
                     </div>
                     <div>
-                        <label className="label">
-                            <span className="text-base label-text">Password</span>
-                        </label>
+
                         <input type="password" placeholder="Enter Password" className="w-full input input-bordered" />
                     </div>
                     <div className='flex gap-2'>
@@ -32,7 +25,7 @@ const Registation = () => {
                         <label htmlFor="checkbox">I agree to privacy police & teams</label>
                     </div>
                     <div>
-                        <button className="btn btn-block">Sign Up</button>
+                        <button className="btn btn-block text-white capitalize  bg-[#1877f2] hover:bg-[#1877f2]">Sign up</button>
                     </div>
                     <span>Already have an account ?
                         <a href="#" className="text-blue-600 hover:text-blue-800 hover:underline">Login</a></span>
